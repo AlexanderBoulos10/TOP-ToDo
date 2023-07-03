9fd63246db24d9375180c1381380ca133fc20e62
Alexander Boulos <alexander.boulos10@gmail.com>
2023-07-02 23:47:52 -0400

    added dom controls for projects and showing tasks

---
+++ src/domTaskFunctions.js
@@ -0,0 +1,5 @@
+import taskFunctions from "./task";
+import projectFunctions from "./project";
+
+const nameOfPage = document.querySelector(".nameOfPage");
+const submitTask = document.querySelector(".submit");
